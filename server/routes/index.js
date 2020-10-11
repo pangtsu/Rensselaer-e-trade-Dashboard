584430183720aeb887aec83b53130ecfedd7b3dc
pangtsu <linj14@rpi.edu>
2020-10-11 18:55:17 -0400

    updated app entry

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../queries');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/api/item/:id', db.getSingleItem);
+router.get('/api/item', db.createItem);
+router.get('/api/item/:id', db.updateItem);
+router.get('/api/item/:id', db.removeItem);
+
 
 module.exports = router;
