efad1c80540f1fc8637dcf3f39f3544955ea54cb
asurashen <asurashen8@gmail.com>
2020-12-05 23:13:40 -0500

    add handle functions

--- frontend/src/components/Sell.tsx
+++ frontend/src/components/Sell.tsx
@@ -22,6 +22,7 @@ export interface State {
   category: string;
   name: string;
   price: number;
+  description: string;
 }
 export default class Sell extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -31,13 +32,16 @@ export default class Sell extends React.Component<Props, State> {
       loading: false,
       category: "",
       name: "",
-      price: -1
+      price: -1,
+      description: ""
     };
     this.showModal = this.showModal.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
     this.handleOk = this.handleOk.bind(this);
     this.onCategoryChange = this.onCategoryChange.bind(this);
-    this.onValuesChange = this.onValuesChange.bind(this);
+    this.onDescriptionChange = this.onDescriptionChange.bind(this);
+    this.onPriceChange = this.onPriceChange.bind(this);
+    this.onNameChange = this.onNameChange.bind(this);
   }
 
   private showModal() {
@@ -47,30 +51,37 @@ export default class Sell extends React.Component<Props, State> {
   }
 
   private handleOk() {
-    //this.setState({ loading: true });
-    //setTimeout(() => {
-    //  this.setState({ loading: false });
-    //}, 3000);
     const params = {
       itemName: this.state.name,
       category: this.state.category,
       price: this.state.price
     };
+    console.log(params)
     this.props.onSubmitCallBack(params);
   }
 
   private handleCancel() {
-    this.setState({ visible: false, name: "", category: "", price: -1 });
+    this.setState({ visible: false, name: "", category: "", price: -1, description: "" });
   }
+
+  // event triggers for input changes
+
+  private onNameChange = ({ target: { value } }) => {
+    const val = {value}
+    this.setState({ name: val.value })
+  };
 
   private onCategoryChange(value: any) {
     this.setState({ category: value });
   }
 
-  // event triggers for input changes
-  private onValuesChange = (changedValues: any, allValues: any) => {
-    console.log(changedValues);
-    console.log(allValues);
+  private onPriceChange(value: any) {
+    this.setState({ price: value });
+  }
+
+  private onDescriptionChange = ({ target: { value } }) => {
+    const val = {value}
+    this.setState({ description: val.value })
   };
 
   render() {
@@ -100,34 +111,51 @@ export default class Sell extends React.Component<Props, State> {
             </Button>
           ]}
         >
-          <form>
-           
-            <label>Product Name   
-              <input type = "text" name = "name" />
-            </label> 
-
-            <label>Select a category
+          <Form>
+            <Form.Item
+              name="name"
+              label="Product Name"
+              rules={[{ required: true }]}
+            >
+              <Input.TextArea rows={1} onChange = {this.onNameChange}/>
+            </Form.Item>
+            <Form.Item
+              name="category"
+              label="Product Type"
+              rules={[{ required: true }]}
+            >
               <Select
                 placeholder="Please select category"
                 onChange={this.onCategoryChange}
               >
-                <Option value="furnitures">furnitures</Option>
-                <Option value="electronic devices">electronic devices</Option>
-                <Option value="clothing-accesory">clothing&accesory</Option>
-                <Option value="books">books</Option>
-                <Option value="school-lab">school&lab</Option>
+                <Option value="furnitures">Furnitures</Option>
+                <Option value="electronic devices">Electronic Devices</Option>
+                <Option value="clothing-accesory">Clothing & Accesory</Option>
+                <Option value="school-lab">School & Lab</Option>
               </Select>
-            </label>  
-
-            <label>Price
-              <InputNumber />
-            </label> 
-
-            <label>Description
-              <Input.TextArea />
-            </label>
-
-          </form>
+            </Form.Item>
+            <Form.Item
+              name="price"
+              label="Price"
+              rules={[
+                {
+                  required: true,
+                  type: "number",
+                  min: 0,
+                  max: 20000
+                }
+              ]}
+            >
+              <InputNumber onChange = {this.onPriceChange} />
+            </Form.Item>
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[{ required: false }]}
+            >
+              <Input.TextArea rows={4} onChange = {this.onDescriptionChange} />
+            </Form.Item>
+          </Form>
         </Modal>
       </>
     );
