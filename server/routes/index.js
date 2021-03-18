eb243055d895a1aef1d3a19696385b9df40ee778
pangtsu <linj14@rpi.edu>
2021-03-17 20:51:41 -0400

    updated router and env

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,18 +2,20 @@ var express = require('express');
 var router = express.Router();
 let upload = require('../config/multer.config.js');
 const fileWorker = require('../controllers/file.controller.js');
-let path = __basedir + '/views/';
+const itemWorker = require('../controllers/queries');
 
-var db = require('../queries');
-/* GET home page. */
 
-router.get('/api/item/:key', db.getItem);
-router.post('/api/item', db.createItem);
-router.put('/api/item/:id', db.updateItem);
-router.delete('/api/item/:id', db.removeItem);
+/* items */
+
+router.get('/api/item/:key', itemWorker.getItem);
+router.post('/api/item', itemWorker.createItem);
+router.put('/api/item/:id', itemWorker.updateItem);
+router.delete('/api/item/:id', itemWorker.removeItem);
+
+/* files */
 
 router.post('/api/file/upload', upload.single("file"), fileWorker.uploadFile);
-router.post('/api/file/multiple/upload', upload.array('files', 4), fileWorker.uploadMultipleFiles); 
+router.get('/api/file/info', fileWorker.listAllFiles);
 router.get('/api/file/:id', fileWorker.downloadFile);
  
 
